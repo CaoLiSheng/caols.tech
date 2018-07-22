cb7ecf1ea75a8cea22d0f17472ca6d0cba62bd5f
cao <cao@caodeMBP.lan>
2018-07-22 19:35:16 +0800

    fix blank line gap

--- webapp/config/webpack.config.dev.js
+++ webapp/config/webpack.config.dev.js
@@ -19,6 +19,8 @@ module.exports = {
       'react-dom',
       'react-router',
       'react-router-dom',
+      'react-router-config',
+      'react-router-redux'
     ],
     app: path.resolve(__dirname, '../src/index.js'),
   },
